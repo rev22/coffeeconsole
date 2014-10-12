903a96159ff62af3f056449d790bec3f57bc7923
Michele Bini <michele.bini@gmail.com>
2014-10-12 06:32:43 +0200

    misc progress

---
+++ ChromiumCoffeeConsole/bookmarklet.js
@@ -0,0 +1,2 @@
+/* alert(document.title); */
+(function(s){s.id='coffeeconsoleinject';s.src='http://rev22.github.io/coffeeconsole/inject.js';document.body.appendChild(s)})(document.createElement('script'));
